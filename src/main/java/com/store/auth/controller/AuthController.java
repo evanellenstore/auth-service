31d9077727dc5bf83efeb3707d2eb78683093c48
Amresh Verma <geniousamresh@gmail.com>
2026-01-15 11:44:22 +0530

    init

---
+++ src/main/java/com/store/auth/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.store.auth.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.store.auth.dto.AuthResponse;
+import com.store.auth.dto.AuthValidationResponse;
+import com.store.auth.dto.LoginRequest;
+import com.store.auth.service.AuthService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService service;
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody LoginRequest request) {
+        return service.login(request);
+    }
+
+    @PostMapping("/validate")
+    public AuthValidationResponse validate(@RequestHeader("Authorization") String token) {
+        AuthValidationResponse res = service.validateToken(token.replace("Bearer ", ""));
+
+        log.info("Auth validation response: {}", res);
+        return res;
+    }
+
+
+}
---
+++ src/main/java/com/store/auth/dto/UserDto.java
@@ -0,0 +1,11 @@
+package com.store.auth.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private String username;
+    private String password;
+    private String role;
+    private Boolean active;
+}
